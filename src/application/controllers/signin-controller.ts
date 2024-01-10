d8cb45a01efa32a425578fb755f718dcc9e1c839
black-adm <mattheuscontato17@gmail.com>
2024-01-10 14:14:25 -0300

    feat: sign in controller

---
+++ src/application/controllers/signin-controller.ts
@@ -0,0 +1,33 @@
+import { z, ZodError } from 'zod';
+import { IController, IRequest, IResponse } from '../interfaces/controller';
+import { SignInUseCase } from '../use-cases/signin-use-case';
+
+const schema = z.object({
+  email: z.string().email().min(1),
+  password: z.string().min(6),
+});
+
+export class SignInController implements IController {
+  constructor(private readonly signInUseCase: SignInUseCase) {}
+
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { email, password } = schema.parse(body);
+
+      const { accessToken } = await this.signInUseCase.execute({ email, password });
+      return {
+        statusCode: 200,
+        body: { accessToken },
+      };
+
+    } catch(error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        };
+      }
+      throw error;
+    }
+  }
+}
